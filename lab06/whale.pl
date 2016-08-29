e150fe009d834142d6bbfa281efd6080a4db3aa8
Surya Avala <suryaavala.dev@gmail.com>
2016-08-30 01:48:56 +1000

    whales.pl final before autotests

--- lab06/whale.pl
+++ lab06/whale.pl
@@ -10,11 +10,43 @@ use warnings;
 my $key_word = $ARGV[0];
 my @lines;
 
+#@whales = [[$whale_name,$count],...,[$whale_name,$count]]
+my @whales;
+
 while (<STDIN>)
 {
   /\S/ or last;
   push @lines, $_;
 }
 
-print "$key_word\n";
-print @lines;
+#print "$key_word\n";
+#print @lines;
+
+foreach my $line (@lines)
+{
+  # body...
+  my @words = split(" ",$line);
+  my $count = $words[0];
+  my $whale_name = join(" ",@words[1..$#words]);
+  my @pod = ($whale_name, $count);
+  push @whales, \@pod;
+  #push @whales, \@($whale_name,$count);
+}
+
+my $pods;
+my $individuals = 0;
+
+foreach (@whales)
+{
+  # body...
+  my @pod = @$_;
+  if ($pod[0] eq $key_word)
+  {
+    $pods++;
+    $individuals = $individuals + $pod[1];
+  }
+  #print "@pod[0], @pod[1]\n";
+
+}
+
+print "$key_word observations: $pods pods, $individuals individuals\n";
