0990959ba77f85e91e0be5be447b19ea9aa22ef8
Surya Avala <suryaavala.dev@gmail.com>
2016-08-30 03:39:33 +1000

    prereq.pl done input,at wget html page

--- lab06/prereq.pl
+++ lab06/prereq.pl
@@ -2,3 +2,19 @@
 #Surya Avinash Avala, z5096886
 #COMP9041 Software Construction, 16s2, lab06
 #prereq.pl
+
+use strict;
+use warnings;
+
+my $course = $ARGV[0];
+
+print "$course\n";
+
+my $url = "http://www.handbook.unsw.edu.au/postgraduate/courses/2016/$course.html";
+open F, "wget -q -O- $url|" or die;
+
+while (my $line = <F>) {
+    print $line;
+}
+
+close(F);
