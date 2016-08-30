3e905c083ace852129bb9d5eb37c86b7c5dc2148
Surya Avala <suryaavala.dev@gmail.com>
2016-08-30 12:43:58 +1000

    trail.pl; temp file added to lab06

---
+++ lab06/trail.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+
+# my %whale_base;
+#
+# $whale_base{$ARGV[0]} = $ARGV[1];
+#
+#
+# foreach my $whale (keys %whale_base) {
+#
+#   # body...
+#   print $whale, $whale_base{$whale};
+# }
+#
+# print "\n";
+# my $whale = $ARGV[0];
+# $whale_base{$whale} =  $whale_base{$whale} + 55;
+#
+# print $whale, $whale_base{$whale};
+
+my $s = "Surya AvinAsh AVA:AlA";
+
+if ($s =~ /.*[A]$/)
+{
+  print "s\n"
+}
+else
+{
+  print "n\n"
+}
