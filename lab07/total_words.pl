5b3133411cdfd87c61ff8b5d587925c9220eb7ea
Surya Avala <suryaavala.dev@gmail.com>
2016-09-12 20:56:23 +1000

    total_words.pl working for most part, pending testing

---
+++ lab07/total_words.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+#Surya Avinash Avala, z5096886
+#COMP9041 Software Construction, 16s2, lab06
+#prereq.pl
+
+
+use strict;
+use warnings;
+
+#my @file_name = <STDIN>;
+
+#print "@file_name\n";
+my $words = 0;
+
+foreach my $x (<STDIN>)
+{
+  #print "$x\n******\n";
+  #my @temp = split(/[a-z]*/i, $x);
+  #chomp ($x);
+
+  #print "$x\n*******\n";
+  $x =~ s/[^a-zA-Z]/ /g;
+  $x =~ s/\s+/ /g;
+
+  my @temp = split(/ /, $x);
+
+
+
+  #print "$x\n@temp\n$#temp\n";
+  $words += $#temp+1;
+
+
+
+}
+print "$words\n";
