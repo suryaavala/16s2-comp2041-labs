6bfa081f32517cf8d7c63491e9a204dd612f3064
Surya Avala <suryaavala.dev@gmail.com>
2016-09-14 11:16:32 +1000

    added courses.pl

---
+++ lab08/courses.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl -w
+#Surya Avinash Avala, z5096886
+#COMP9041 Software Construction, 16s2, lab06
+#courses.pl
+
+use strict;
+use warnings;
+
+my $code = $ARGV[1];
+
+print "$code\n";
