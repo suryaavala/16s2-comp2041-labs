1ac09a28e8d13b7523ddabbbf69a8c7ac2b10691
Surya Avala <suryaavala.dev@gmail.com>
2016-09-14 12:40:41 +1000

    generalised pattern in courses.pl

--- lab08/courses.pl
+++ lab08/courses.pl
@@ -9,14 +9,7 @@ use warnings;
 my $course_code = $ARGV[0];
 my $url =  "http://www.timetable.unsw.edu.au/current/$course_code".""."KENS.html";
 my @courses;
-
-sub not_present
-{
-    my $a = grep(/@_/,@courses);
-    #print "@_,$a\n";
-    return 0 if $a;
-    return 1;
-}
+my $pattern = $course_code.""."[0-9]{4}";
 
 sub extract_http
 {
@@ -29,7 +22,9 @@ sub extract_http
   while (my $line = <F>)
   {
 
-    my $temp_code = $& if $line =~ m/$course_code[0-9]{4}/g;
+    #my $temp_code = $& if $line =~ m/$course_code[0-9]{4}/g;
+    my $temp_code = $& if $line =~ m/$pattern/g;
+
     #print "$temp_code\n" if $temp_code;
     #push @courses, $temp_code if ($temp_code and (grep $temp_code, @courses));
     #push @courses, $temp_code if ($temp_code and &not_present($temp_code));
@@ -45,6 +40,6 @@ sub extract_http
 
 &extract_http();
 @courses = sort { lc($a) cmp lc($b) } @courses;
-print "$_\n" foreach(@courses) 
+print "$_\n" foreach(@courses)
 
 #if (10 and 1) { print "1\n";}
